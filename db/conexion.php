2c0febcf0facc7e59775705ea52c116152809e6c
innovacionfyc <innovacionti@fycconsultores.com>
2025-08-08 15:56:22 -0500

    Primer deploy para probar el funcionamiento del envio de correo

--- db/conexion.php
+++ db/conexion.php
@@ -1,14 +1,19 @@
 <?php
-$host = "localhost";
-/* $user = "root";
-$pass = ""; */
-$usuario = "ErickCer1818";          
-$contrasena = "ElManCertifica18#";
-$dbname = "inscritos_db";
+// Muestra errores solo mientras pruebas en el server.
+// Quita estas 2 líneas cuando todo funcione.
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+/**
+ * PRODUCCIÓN (Plesk)
+ */
+$host = 'localhost';              // en Plesk suele ir bien 127.0.0.1
+$user = 'ErickCer1818';           // <-- tu usuario real
+$pass = 'ElManCertifica18#';      // <-- tu contraseña real
+$dbname = 'inscritos_db';         // <-- tu base
 
 $conn = new mysqli($host, $user, $pass, $dbname);
-
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+if ($conn->connect_errno) {
+    die('Error de conexión: ' . $conn->connect_error);
 }
-?>
+$conn->set_charset('utf8');
